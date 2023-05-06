0f886703b8b54a28a2990bf24833d5f1a646e07f
Amira-Helmy <amirahelmy11223344@gmail.com>
2023-05-06 21:41:39 +0200

    Ensure MySQL connection in app.py when the server starts

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,12 @@ from backend.database_operations.user import UserDAO, UserLogin, UserRegistratio
 
 app = Flask(__name__)
 db_name = 'ai_trainer'
+credentials =  MysqlCredentials.get_mysql_credentials()
+connection = DatabaseConnection.connect_to_mysql_server(credentials['DBN'],credentials['mysql_username'],credentials['mysql_pwd'],credentials['mysql_host'])
+user_login = UserLogin(connection, db_name)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
