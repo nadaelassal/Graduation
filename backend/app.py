eb22b02c0490d13862a2cbad4ba48d4f48271b01
Amira-Helmy <amirahelmy11223344@gmail.com>
2023-05-06 21:42:54 +0200

    Create endpoints for login and register and reset password.

--- backend/app.py
+++ backend/app.py
@@ -12,6 +12,41 @@ user_login = UserLogin(connection, db_name)
 
 
 
+@app.route('/check_user', methods=['POST'])
+def check_user():
+    email = request.json['email']
+    user_info = user_login.user_dao.retrieve_user_information(email)
+    if user_info is not None:
+        return jsonify({'exists': True})
+    else:
+        return jsonify({'exists': False})
 
+@app.route('/signin', methods=['POST'])
+def login():
+    email = request.json['email']
+    password = request.json['password']
+    authenticated = UserLogin(connection,db_name ).authenticate_user(email, password)
+    if authenticated:
+        # login successful
+        return jsonify({'status':True,'message': 'Login successful'})
+    else:
+        # login failed
+        return jsonify({'status':False,'message': 'Login failed'})
+    
+user_registration = UserRegistration(connection,db_name )
+
+@app.route('/register', methods=['POST'])
+def register():
+    email = request.json['email']
+    username = request.json['username']
+    password = request.json['password']
+
+    registered = user_registration.register_user(email, username, password)
+
+    if registered:
+        return jsonify ({'status':True ,'message' :f"email {email} registered successfully"})
+    else:
+        return jsonify ({'status':False ,'message' :f"Email {email} is already taken"})
+    
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
